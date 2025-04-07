2821308b6622e2875260ac9d1e2f9b71ea746a05
Mokhless Kadiri <102821@glr.nl>
2025-04-07 23:27:52 +0000

    tot niet toe

---
+++ data.js
@@ -0,0 +1,11 @@
+
+let products = [
+
+    {
+        id: 1,
+        name: 'Product 1', 
+        Description: 'loremloremloremloremloremloremloremloremloremlorem',
+        price: 40,
+    }
+    
+    ]
---
+++ script.js
@@ -0,0 +1,8 @@
+let navMenu = document.querySelector('nav ul')
+let burgeMenu = document.querySelector('.burger-menu');
+
+
+burgeMenu.addEventListener('click', ()=> {
+    navMenu.classList.toggle('active');
+    console.log(navMenu)
+})
