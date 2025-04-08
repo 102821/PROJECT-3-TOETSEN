41303933bd43a476dd35c4da83e00d19ba0608da
Mokhless Kadiri <102821@glr.nl>
2025-04-08 12:21:27 +0000

    a

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let burgeMenu = document.querySelector('.burger-menu');
 burgeMenu.addEventListener('click', ()=> {
     navMenu.classList.toggle('active');
     console.log(navMenu)
-})
+});
 
 // Details-product pagina
 // function showProduct(productN) {
@@ -15,59 +15,60 @@ burgeMenu.addEventListener('click', ()=> {
 // }
 // let cartDiv = document.querySelectorAll('');
 
-let form = document.querySelector('.cart-page-container .form');
+let form = document.getElementsByClassName('form')[0];
+console.log(form)
 
-form.addEventListener('submit', (e)=> {
-    e.preventDefault();
-    // if () {
+// form.addEventListener('submit', (e)=> {
+//     e.preventDefault();
+//     // if () {
 
-    // }
-    let valid = true;
+//     // }
+//     let valid = true;
 
-    let errorDivs = document.querySelectorAll('.error');
-    errorDivs.forEach((div)=> {
-        div.textContent = '';
-    })
+//     let errorDivs = document.querySelectorAll('.error');
+//     errorDivs.forEach((div)=> {
+//         div.textContent = '';
+//     })
 
-    const fName = document.querySelector('#firstName').value;
-    console.log(fName);
-    if (!/^[A-Za-z]{2,}$/.test(fName) && fName !== '') {
-        document.querySelector('#fNameError').textContent = 'Ongeldig voornaam !';
-        valid = false
-    }
+//     const fName = document.querySelector('#firstName').value;
+//     console.log(fName);
+//     if (!/^[A-Za-z]{2,}$/.test(fName) && fName !== '') {
+//         document.querySelector('#fNameError').textContent = 'Ongeldig voornaam !';
+//         valid = false
+//     }
     
-    const lName = document.querySelector('#lastName').value;
-    console.log(lName)
+//     const lName = document.querySelector('#lastName').value;
+//     console.log(lName)
 
-    if (!/^[A-Za-z]{2,}$/.test(lName) && lName !== '') {
-        document.querySelector('#lNameError').textContent = 'Ongeldig achternaam !';
-        valid = false
-    }
+//     if (!/^[A-Za-z]{2,}$/.test(lName) && lName !== '') {
+//         document.querySelector('#lNameError').textContent = 'Ongeldig achternaam !';
+//         valid = false
+//     }
 
-    // /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    const email = document.querySelector('#email').value;
-    // if (!/^[A-Za-z]{2,}$/.test(email) && email !== '') {
-    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email) && email !== '') {
-        document.querySelector('#emailError').textContent = 'Ongeldig email !';
-        valid = false
-    }
-
-
-    // /^[A-Za-z\s]+ \d+[a-zA-Z]?$/ 
-    const address = document.querySelector('#address').value;
-    if (!/^[A-Za-z\s]+ \d+[a-zA-Z]?$/.test(address) && address !== '') {
-        document.querySelector('#addressError').textContent = 'Ongeldig adres ((noordplein) (44) ) !';
-        valid = false
-    }
+//     // /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+//     const email = document.querySelector('#email').value;
+//     // if (!/^[A-Za-z]{2,}$/.test(email) && email !== '') {
+//     if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email) && email !== '') {
+//         document.querySelector('#emailError').textContent = 'Ongeldig email !';
+//         valid = false
+//     }
 
 
-    const postCode = document.querySelector('#postCode').value;
-    if (!/^[1-9][0-9]{3}\s?[A-Za-z]{2}$/.test(postCode) && postCode !== '') {
-        document.querySelector('#postCodeError').textContent = 'ongeldig postcode !';
-        valid = false
-    }
+//     // /^[A-Za-z\s]+ \d+[a-zA-Z]?$/ 
+//     const address = document.querySelector('#address').value;
+//     if (!/^[A-Za-z\s]+ \d+[a-zA-Z]?$/.test(address) && address !== '') {
+//         document.querySelector('#addressError').textContent = 'Ongeldig adres ((noordplein) (44) ) !';
+//         valid = false
+//     }
 
-    if (valid) {
-        alert(`${fName.toUpperCase()}, Bestteling is gelukt :)`)
-    }
-})
+
+//     const postCode = document.querySelector('#postCode').value;
+//     if (!/^[1-9][0-9]{3}\s?[A-Za-z]{2}$/.test(postCode) && postCode !== '') {
+//         document.querySelector('#postCodeError').textContent = 'ongeldig postcode !';
+//         valid = false
+//     }
+
+//     if (valid) {
+//         alert(`${fName.toUpperCase()}, Bestteling is gelukt :)`)
+//     }
+// })
